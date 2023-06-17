ddf8d2188e5c507c51b950147e6c670920892c2c
千橘 雫霞 <i@qianjunakasumi.moe>
2023-06-17 14:37:15 +0900

    chore:page_facing_up:: update copyright email address

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-// Copyright (c) 2022 qianjunakasumi <i@qianjunakasumi.ren>
-//                    qianjunakasumi <qianjunakasumi@outlook.com>
-//                    [qianjunakasumi] 千橘雫霞(https://github.com/qianjunakasumi)
+// Copyright (c) 2022 qianjunakasumi <{official, i}@qianjunakasumi{.moe, @{gmail, outlook}.com}>
 //
-//      This Source Code Form is subject to the terms of the Mozilla Public
-//      License, v. 2.0. If a copy of the MPL was not distributed with this
-//      file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://github.com/qianjunakasumi/ZhiXueRank/blob/main/LICENSE.
 
 // ==UserScript==
 // @name         智学网排行榜
